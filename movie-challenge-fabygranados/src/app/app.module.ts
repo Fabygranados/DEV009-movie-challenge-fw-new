53976945e8f730d1264e59f29cffd7d1a592c9e3
Fabiola Granados <faby.granados@gmail.com>
2023-10-18 15:38:08 -0700

    Se modificaron modulos, agregando y borrando

--- movie-challenge-fabygranados/src/app/app.module.ts
+++ movie-challenge-fabygranados/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserAnimationsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToolbarComponent
+    ToolbarComponent,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
